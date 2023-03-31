ea29cd08057cf58b97e1b9c5e9ac801db8269c4f
Ranccat <wogurjo98@gmail.com>
2023-04-01 00:15:24 +0900

    please merge

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -8,19 +8,19 @@ public class MapGenerator : MonoBehaviour
 {
     public GameObject wallPrefab;
     
-    private const int MapSize = 50;
+    const int MapSize = 50;
     Room[,] Rooms = new Room[MapSize, MapSize];
     int[,] Maze = new int[MapSize * 2 + 1, MapSize * 2 + 1];
-    private List<Tuple<int, int>> Visited = new List<Tuple<int, int>>();
+    List<Tuple<int, int>> Visited = new List<Tuple<int, int>>();
 
     class Room
     {
-        private int x;
-        private int y;
-        private Tuple<int, int> myDir;
-        private List<Tuple<int, int>> dir = new List<Tuple<int, int>>();
+        int x;
+        int y;
+        Tuple<int, int> myDir;
+        List<Tuple<int, int>> dir = new List<Tuple<int, int>>();
         public Queue<Tuple<int, int>> randomizedDir = new Queue<Tuple<int, int>>();
-        private Random _random = new Random();
+        Random _random = new Random();
         
         public Room(int x, int y)
         {
@@ -58,7 +58,6 @@ public class MapGenerator : MonoBehaviour
     
     int[,] DrawMap()
     {
-        // Initialize the whole map with class: Room
         for (int x = 0; x < MapSize; x++)
         {
             for (int y = 0; y < MapSize; y++)
@@ -66,7 +65,7 @@ public class MapGenerator : MonoBehaviour
                 Rooms[x, y] = new Room(x, y);
             }
         }
-        // Initialize the grid where 1 stands for wall and 0 for path
+        
         int mazeLength = MapSize * 2 + 1;
         for (int x = 0; x < mazeLength; x++)
         {
@@ -86,8 +85,10 @@ public class MapGenerator : MonoBehaviour
         Tuple<int, int> currentPos = currentRoom.GetCurrentPos();
         int currentX = currentPos.Item1;
         int currentY = currentPos.Item2;
+        
         Visited.Add(new Tuple<int, int>(currentX, currentY));
         Maze[currentX * 2 + 1, currentY * 2 + 1] = 0;
+        
         while (currentRoom.randomizedDir.Count != 0)
         {
             Tuple<int, int> nextDir = currentRoom.GetNextPos();
